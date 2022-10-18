a6e197618e406befbe180b82ad835b96fd7647d5
Kayla Justice <kaylarjustice@gmail.com>
2022-10-18 09:58:32 -0500

    Added async/await to POST route to handle immediate display of added notes.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const path = require('path');
 const uuid = require('./helpers/uuid');
 const db = require('./db/db.json');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('public'));
-
 
 // Returns the `index.html` file
 app.get('/', (req, res) => {
@@ -28,7 +30,7 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => res.json(db));
 
 // Receives new note to save on request body, adds it to `db.json` file, returns the new note to the client
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', async (req, res) => {
   res.send("Received");
 
   // Destructuring assignment for the items in req.body
@@ -44,7 +46,7 @@ app.post('/api/notes', (req, res) => {
     };
 
     // Retrieve what data is already in `db.json`, assign it
-    let noteData = fs.readFileSync('./db/db.json');
+    let noteData = await readFileAsync('./db/db.json', 'utf8');
     // Parse the data from `db.json`, assign it
     let noteArray = JSON.parse(noteData);
     // push changes to array
@@ -52,7 +54,7 @@ app.post('/api/notes', (req, res) => {
     
     // Append the data to the `db.json` file  
     let newNote = JSON.stringify(noteArray);
-    fs.writeFileSync('./db/db.json', newNote);
+    await writeFileAsync('./db/db.json', newNote);
   
     const response = {
     status: 'success',
@@ -60,14 +62,13 @@ app.post('/api/notes', (req, res) => {
     };
     console.log(response);  
   };
-  // How to return note immediately upon saving/adding?
-  // What is the `+` versus `save` feature? Temp add versus store? How to handle?
   return note;     
 });
 
 // Receives a query parameter containing id of note to delete, reads all notes in `db.json`, removes note with assigned `id`, rewrites notes to `db.json`
 app.delete('api/notes/:id', (req, res) => {
-//   const selectedNote = req.params.id;
+  const selectedNote = req.params.id;
+  res.send("DELETE request called.")   
 
 //   for (let i = 0; i < db.length; i++) {
 //     if (selectedNote === db[i].id) {
